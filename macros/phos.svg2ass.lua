c9157695710f1de0fe6a657602797f09fbd15bc0
PhosCity <minatouzumaki8613@gmail.com>
2022-05-11 18:23:31 +0545

    [Svg2ass] change config from depctrl to a-mo

--- macros/phos.svg2ass.lua
+++ macros/phos.svg2ass.lua
@@ -2,21 +2,24 @@
 script_name = "svg2ass"
 script_description = "Script that uses svg2ass to convert svg files to ass lines"
 script_author = "PhosCity"
-script_version = "0.0.5"
+script_version = "0.0.6"
 script_namespace = "phos.svg"
 
 DependencyControl = require("l0.DependencyControl")
-local depRec = DependencyControl({})
-
-local config = depRec:getConfigHandler({
-	svg2ass_path = "svg2ass",
-	svg2ass_parameters = "",
-	user_tags = "\\bord0\\shad0",
+local depRec = DependencyControl({
+	{
+		"a-mo.ConfigHandler",
+		version = "1.1.4",
+		url = "https://github.com/TypesettingTools/Aegisub-Motion",
+		feed = "https://raw.githubusercontent.com/TypesettingTools/Aegisub-Motion/DepCtrl/DependencyControl.json",
+	},
 })
 
-local function config_setup()
-	local CONFIG_GUI = {
-		{
+ConfigHandler = depRec:requireModules()
+
+local CONFIG_GUI = {
+	main = {
+		label1 = {
 			x = 0,
 			y = 0,
 			width = 5,
@@ -24,16 +27,16 @@ local function config_setup()
 			class = "label",
 			label = "Absoulute path of svg2ass executable:",
 		},
-		{
+		svgpath = {
 			x = 0,
 			y = 1,
 			width = 20,
 			height = 3,
-			name = "svgpth",
 			class = "edit",
-			text = config.c.svg2ass_path,
+			config = true,
+			value = "svg2ass",
 		},
-		{
+		label2 = {
 			x = 0,
 			y = 4,
 			width = 5,
@@ -41,7 +44,7 @@ local function config_setup()
 			class = "label",
 			label = "svg2ass options:",
 		},
-		{
+		label3 = {
 			x = 0,
 			y = 5,
 			width = 5,
@@ -49,7 +52,7 @@ local function config_setup()
 			class = "label",
 			label = "Default options already used: -S, -E, -T ",
 		},
-		{
+		label4 = {
 			x = 0,
 			y = 6,
 			width = 10,
@@ -57,16 +60,16 @@ local function config_setup()
 			class = "label",
 			label = "No processing of options below will be done. Garbage in, Garbage out.",
 		},
-		{
+		svgopt = {
 			x = 0,
 			y = 7,
 			width = 20,
 			height = 3,
-			name = "svgopt",
 			class = "edit",
-			text = config.c.svg2ass_parameters,
+			config = true,
+			value = "",
 		},
-		{
+		label5 = {
 			x = 0,
 			y = 10,
 			width = 5,
@@ -74,7 +77,7 @@ local function config_setup()
 			class = "label",
 			label = "Custom ASS Tags:",
 		},
-		{
+		label6 = {
 			x = 0,
 			y = 11,
 			width = 10,
@@ -82,7 +85,7 @@ local function config_setup()
 			class = "label",
 			label = "Default tags added automatically: \\an7\\pos(0,0)\\p1",
 		},
-		{
+		label7 = {
 			x = 0,
 			y = 12,
 			width = 10,
@@ -90,25 +93,29 @@ local function config_setup()
 			class = "label",
 			label = "No processing of tags below will be done. Garbage in, Garbage out.",
 		},
-		{
+		usertags = {
 			x = 0,
 			y = 13,
 			width = 20,
 			height = 3,
-			name = "usrtgs",
 			class = "edit",
-			text = config.c.user_tags,
+			config = true,
+			value = "\\bord0\\shad0",
 		},
-	}
-	Buttons = { "Save", "Cancel" }
-	Pressed, Res = aegisub.dialog.display(CONFIG_GUI, Buttons)
-	if Pressed == "Cancel" then
-		aegisub.cancel()
-	elseif Pressed == "Save" then
-		config.c.svg2ass_path = Res.svgpth
-		config.c.svg2ass_parameters = Res.svgopt
-		config.c.user_tags = Res.usrtgs
+	},
+}
+
+local config = ConfigHandler(CONFIG_GUI, depRec.configFile, false, script_version, depRec.configDir)
+
+-- Modify config
+local function config_setup()
+	config:read()
+	config:updateInterface("main")
+	local button, result = aegisub.dialog.display(CONFIG_GUI.main)
+	if button then
+		config:updateConfiguration(result, "main")
 		config:write()
+		config:updateInterface("main")
 	end
 end
 
@@ -216,6 +223,7 @@ local function clip_to_iclip(text)
 	return text
 end
 
+-- Execute the command
 local function run_cmd(command)
 	local handle = io.popen(command)
 	local result = handle:read("*a")
@@ -231,54 +239,78 @@ local function progress(msg)
 	aegisub.progress.title(msg)
 end
 
+-- Main function
 local function svg2ass(subs, sel, res)
-	config:load()
-	check_svg2ass_exists(config.c.svg2ass_path)
+	-- Read config
+	config:read()
+	config:updateInterface("main")
+	local opt = config.configuration.main
+
+	-- Check if svg2ass executable exists
+	check_svg2ass_exists(opt.svgpath)
+
 	if #sel ~= 1 then
 		aegisub.log(
 			"You must select exactly one line\nThat line's start time, end time and style will be copied to resulting lines."
 		)
 		return
 	end
+
+	-- Select svg file
 	local ffilter = "SVG Files (.svg)|*.svg"
 	local fname = aegisub.dialog.open("Select svg file", "", "", ffilter, false, true)
+
+	-- Initialize new selection(for new llines that were added)
 	local newsel = {}
+
 	for _, i in ipairs(sel) do
 		if subs[i].class == "dialogue" then
 			local line = subs[i]
 			if not fname then
 				aegisub.cancel()
 			else
-				local command = config.c.svg2ass_path
+				-- Generate svg2ass command
+				local command = opt.svgpath
 					.. " -S "
 					.. time2string(line.start_time)
 					.. " -E "
 					.. time2string(line.end_time)
 					.. " -T "
 					.. line.style
-				if config.c.svg2ass_parameters then
-					command = command .. " " .. config.c.svg2ass_parameters
+				if opt.svgopt then
+					command = command .. " " .. opt.svgopt
 				end
 				command = command .. ' "' .. fname .. '"'
+
+				-- Execute the command and grab it's result
 				local result = run_cmd(command)
 
+				-- Count the total number of lines in result
 				local _, line_count = string.gsub(result, "\n", "\n")
-				local inserts = 1
+
+				local inserts = 0
 				local count = 1
 				for j in result:gmatch("[^\n]+") do
+					-- Progressbar
 					progress("Progress Occurs")
 					aegisub.progress.set((count * 100) / line_count)
+
 					local newline = string2line(j)
 					local primary_color = newline.text:match("\\1c&H%x+&"):gsub("\\1c&", "\\c&")
-					local tags = "{\\an7\\pos(0,0)" .. config.c.user_tags .. primary_color .. "\\p1}"
+					local tags = "{\\an7\\pos(0,0)" .. opt.usertags .. primary_color .. "\\p1}"
 					local text = newline.text:gsub("{\\[^}]-}", "")
 					newline.text = tags .. text
+
+					-- Convert shape to clip
 					if res.clip then
 						newline.text = shape_to_clip(newline.text)
 					end
+
+					-- Convert shape to iclip
 					if res.iclip then
 						newline.text = clip_to_iclip(newline.text)
 					end
+
 					subs.insert(sel[1] - inserts, newline)
 					table.insert(newsel, sel[1] - inserts)
 					inserts = inserts - 1
@@ -298,7 +330,7 @@ local function main(subs, sel)
 			width = 7,
 			height = 5,
 			class = "textbox",
-			value = "have ass, will typeset\nwill also crash Aegisub\nsave file before running",
+			text = "have ass, will typeset\nwill also crash Aegisub\nsave file before running",
 		},
 		{
 			x = 0,
