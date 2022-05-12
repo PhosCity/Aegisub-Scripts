451f06ad28eed78374579a403f1f4d82bf577197
PhosCity <minatouzumaki8613@gmail.com>
2022-05-12 21:49:50 +0545

    [Svg2ass] chore: bump version

--- macros/phos.svg2ass.lua
+++ macros/phos.svg2ass.lua
@@ -2,7 +2,7 @@
 script_name = "svg2ass"
 script_description = "Script that uses svg2ass to convert svg files to ass lines"
 script_author = "PhosCity"
-script_version = "0.0.7"
+script_version = "0.0.8"
 script_namespace = "phos.svg"
 
 DependencyControl = require("l0.DependencyControl")
